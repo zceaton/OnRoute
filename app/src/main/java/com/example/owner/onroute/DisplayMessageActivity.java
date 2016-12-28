2485c663de8c77f0309ef032bd29d01f70372ab3
zceaton <zceaton@buffalo.edu>
2016-12-28 15:45:11 -0500

    Now takes starting location and destination as input strings and stores them

---
+++ app/src/main/java/com/example/owner/onroute/DisplayMessageActivity.java
@@ -0,0 +1,28 @@
+package com.example.owner.onroute;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import static com.example.owner.onroute.MainActivity.*;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_message);
+        layout.addView(textView);
+    }
+}
--- app/src/main/java/com/example/owner/onroute/MainActivity.java
+++ app/src/main/java/com/example/owner/onroute/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.owner.onroute;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText1 = (EditText) findViewById(R.id.start);
+        String startLocation = editText1.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, startLocation);
+        EditText editText2 = (EditText) findViewById(R.id.destination);
+        String destination = editText2.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, destination);
+        startActivity(intent);
     }
 }
